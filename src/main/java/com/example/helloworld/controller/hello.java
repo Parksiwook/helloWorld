e2907386a466b3a57958fa1b7f5377409c087e30
HonamJSH <99239284+HonamJSH@users.noreply.github.com>
2022-07-11 18:15:23 +0900

    gradle MySQL 커넥터 의존성 추가

--- src/main/java/com/example/helloworld/controller/hello.java
+++ src/main/java/com/example/helloworld/controller/hello.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class hello {
 
-
     @GetMapping("/test")
     public String test(){
         return "hello wordl";
